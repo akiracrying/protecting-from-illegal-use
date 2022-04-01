47bab52363aac802b495dd9f21ba1ebd85aaa98e
Timofey Tsvetkov <78951084+weinenwollen@users.noreply.github.com>
2022-04-01 03:30:16 +0300

    prog.c + pas.txt

---
+++ prog.cpp
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void password() {
+	//char* n = (char*)malloc(sizeof(char));
+	char n[50];
+	FILE* file;
+	file = fopen("pas.txt", "r");
+	char word[50];
+	//char *word= (char*)malloc(sizeof(char));;
+	
+	fscanf(file, "%s", word );
+	while(1) {
+		n[0] = '\0';
+		scanf("%s", n);
+		if (!strcmp(n, word))break;
+		printf("wrong one!\n");
+	}
+	printf("nice!\n");
+
+}
+int main() {
+
+	printf("enter the password:\n");
+	password();
+	system("pause");
+	return EXIT_SUCCESS;
+}
